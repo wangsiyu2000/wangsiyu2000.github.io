de407b1c9a08a02c603ca3ea0e55fe9fb4a32a9b
wangsiyu2000 <wangsiyu20000110@gmail.com>
2025-01-23 10:29:51 +0800

    Save

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: ['./layouts/**/*.html'],
+    theme: {
+        extend: {
+            colors: {
+                lavender: '#eeeefa',
+                peach: '#feddd6',
+            },
+            spacing: {
+                gap: '1.5rem',
+                'gap-0.5': '0.75rem',
+                'gap-1.5': '2.25rem',
+            },
+            fontFamily: {
+                corinthia: 'Corinthia',
+                jost: 'jost',
+            },
+        },
+    },
+    plugins: [],
+}
